ec6c2d05ec56ddac0b418afdb562ac3fde717caa
AndyLeeZaiZai <494447856@qq.com>
2022-11-11 02:11:49 +0100

    Simplify Leetcode 9

--- 9.palindrome-number.java
+++ 9.palindrome-number.java
@@ -11,30 +11,15 @@ import java.util.Map;
 class Solution {
     public boolean isPalindrome(int x) {
         // Using math insted of converting Integer to String
-        // Handle special cases
-        if (x < 0) return false;
-        if (x >= 0 && x <= 9) return true;
-
-        // Save the result of mod caculation and remove the units digits by divive the numnber by 10
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int i = 0;
         int temp = x;
-        while (temp >= 1) {
-            map.put(i, temp%10);
-            temp = temp/10;
-            i++;
-        }
-
-        // Reconstruct the number in a reverse order and comapre if it is the same as the original one
-        int factor = 1;
         int reverseOrderNumber = 0;
-        for(int j = map.size() - 1; j >= 0; j--) {
-            reverseOrderNumber = reverseOrderNumber + map.get(j) * factor;
-            factor = factor * 10;
+        while (temp >= 1 ) {
+            reverseOrderNumber = reverseOrderNumber*10 + temp%10;
+            temp = temp/10;
         }
 
         // Only one comparison needed 
-        return reverseOrderNumber == x;
+        return x >= 0 && reverseOrderNumber == x;
     }
 }
 // @lc code=end
